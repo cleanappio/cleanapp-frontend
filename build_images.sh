2097850f6384af407ae49f3a4b7a8babf721977c
Evgeny Koshelev <eko2000@gmail.com>
2025-07-12 01:29:49 +0200

    Add the montenegro api url

--- build_images.sh
+++ build_images.sh
@@ -31,6 +31,7 @@ case ${OPT} in
       NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY="pk_test_51ReIGOFW3SknKzLcSITZxoZi8fySW11iQNY1SAe1dpzVOcHS2U05GlMZ6aQCcSdxILX0r6cm8Lx6yz4U8TR8l6HH00ihXdefVs"
       NEXT_PUBLIC_API_URL="https://devapi.cleanapp.io"
       NEXT_PUBLIC_LIVE_API_URL="https://devlive.cleanapp.io"
+      NEXT_PUBLIC_MONTENEGRO_API_URL="https://devapimontenegro.cleanapp.io"
       TARGET_VM_IP="34.132.121.53"
       break
       ;;
@@ -39,6 +40,7 @@ case ${OPT} in
       NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY="pk_live_51RaMSvF5CkX59Cnm7ZTuIIx0Fg1cQxqilIpOHippAYaVqFMDft3AESH5Ih8aPn4wUFL2VX3Ou9LvwCgqD5O0SDvF00a8ybMiUq"
       NEXT_PUBLIC_API_URL="https://api.cleanapp.io"
       NEXT_PUBLIC_LIVE_API_URL="https://live.cleanapp.io"
+      NEXT_PUBLIC_MONTENEGRO_API_URL="https://apimontenegro.cleanapp.io"
       TARGET_VM_IP="34.122.15.16"
       break
       ;;
