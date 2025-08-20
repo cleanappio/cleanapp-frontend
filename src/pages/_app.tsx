468de09d7cca81bf263b862673f1ff914d78fc0d
Ravi Singh Lodhi <ravisinghlodhi2357@gmail.com>
2025-08-20 13:28:51 +0530

    Add error boundary layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import { useEffect } from 'react';
-import { Toaster } from 'react-hot-toast';
-import { useAuthStore } from '@/lib/auth-store';
-import Layout from '@/components/Layout';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "@/lib/auth-store";
+import Layout from "@/components/Layout";
+import { ErrorBoundary } from "@/components/ErrorBoundary";
 
 export default function App({ Component, pageProps }: AppProps) {
   const checkAuth = useAuthStore((state) => state.checkAuth);
@@ -13,11 +14,11 @@ export default function App({ Component, pageProps }: AppProps) {
   }, [checkAuth]);
 
   return (
-    <>
+    <ErrorBoundary>
       <Layout>
         <Component {...pageProps} />
       </Layout>
       <Toaster position="top-right" />
-    </>
+    </ErrorBoundary>
   );
 }
